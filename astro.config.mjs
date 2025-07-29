e361ab94183ad059edbf778f412955ad560a7220
Jacob Edie <jacobedie1@gmail.com>
2025-07-29 16:25:27 -0400

    added mdx support and the beginning of my Mcdonalds blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
-    output: 'static',
-    vite: {
-        plugins: [tailwindcss()]
-    }
+  output: 'static',
+
+  vite: {
+      plugins: [tailwindcss()]
+  },
+
+  integrations: [mdx()]
 });
